b532142fa9ea79f42365d364ce14e83aad2443eb
edeustace <ed.eustace@gmail.com>
2017-07-03 15:40:41 +0100

    colors logging

--- src/log.ts
+++ src/log.ts
@@ -10,15 +10,40 @@ class NullLogger implements Logger {
   }
 }
 
+const stringToColour = function (str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    // tslint:disable-next-line:no-bitwise
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    // tslint:disable-next-line:no-bitwise
+    var value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+};
+
+
+
 const nullLogger = new NullLogger();
 
 export class ConsoleLogger implements Logger {
   constructor(private name: string) { }
 
-  log(...args: any[]): void {
+  async log(...args: any[]): void {
     // tslint:disable-next-line:no-string-literal
     if (typeof window['console'] !== 'undefined') {
-      return console.log(`[bindi:${this.name}]`, ...args);
+      const color = `${stringToColour(`bindi:${this.name}`)}`;
+
+      // const Color = await import('color');
+      // tslint:disable-next-line:no-bitwise
+      const complement = `white`;
+
+
+      console.log('color: ', color, complement);
+      return console.log(`%c[bindi:${this.name}]`, `color: ${color}; padding: 1px; background-color:${complement}`, ...args);
     }
   }
 }
